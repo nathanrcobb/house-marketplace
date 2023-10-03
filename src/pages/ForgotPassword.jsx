0e36b47446d38bdf5b49b1b33847f688d0a5f041
Nathan Cobb <nathanrcobb@gmail.com>
2023-10-02 18:03:27 -0700

    Google OAuth working

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,7 +1,53 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast } from "react-toastify";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+
 function ForgotPassword() {
+  const [email, setEmail] = useState("");
+
+  const onChange = (e) => setEmail(e.target.value);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Reset email was sent!");
+    } catch (error) {
+      toast.error("Could not send reset email");
+    }
+  };
+
   return (
-    <div>
-      <h1>Forgot Password</h1>
+    <div className="pageContainer">
+      <header className="pageHeader">
+        <p>Forgot Password</p>
+      </header>
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            className="emailInput"
+            value={email}
+            onChange={onChange}
+          />
+          <Link className="forgotPasswordLink" to="/signin">
+            Sign In
+          </Link>
+
+          <div className="signInBar">
+            <div className="signInText">Send Reset Link</div>
+            <button className="signInButton">
+              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 }
